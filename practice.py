c49fd5a6e010f359567ba5f3dd7fd5317691b735
zarir94 <shahriar@gmail.com>
2024-08-21 04:20:34 -0700

    pr

--- practice.py
+++ practice.py
@@ -6,6 +6,10 @@ rows = getLinkRows(0)
 file_ids = [row['url'].split('/u/')[1].split('/')[0].split('?')[0] for row in rows]
 random.shuffle(file_ids)
 print(len(file_ids))
-# for i in range(30):
-    # asyncio.run(getAllFileData(file_ids), debug=False)
+
+for i in range(30):
+    data = asyncio.run(getAllFileData(file_ids), debug=False)
+    for fid, fd in data.items():
+        if not fd:
+            print(f'Error in file {fid}')
 
