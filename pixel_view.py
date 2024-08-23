10717dfa312081ce7443f2298284451cd1477aee
zarir94 <shahriar@gmail.com>
2024-08-23 03:50:37 -0700

    f

--- pixel_view.py
+++ pixel_view.py
@@ -22,7 +22,7 @@ async def view_pixel_drain(file_id, proxy_url=None):
                       extra_headers={'Cookie': 'pd_auth_key=83jdid8d83iejd8e84udh73'},
                       **kwargs) as websocket:
             await websocket.send('{"type":"file_stats","data":{"file_id":"' + file_id + '"}}')
-            message = await asyncio.wait_for(websocket.recv(), 120)
+            message = await asyncio.wait_for(websocket.recv(), 60)
             # print(f"Received: {message.strip()}")
     except: pass
 
